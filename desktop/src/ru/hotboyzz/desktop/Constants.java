5a8fed9889c575bc0303236c550503c304871b66
Artem <klimenkocpp@icloud.com>
2017-12-02 09:16:15 +0600

    added default classes.

---
+++ desktop/src/ru/hotboyzz/desktop/Constants.java
@@ -0,0 +1,7 @@
+package ru.hotboyzz.desktop;
+
+/**
+ * @author Velkonost
+ */
+public class Constants {
+}
---
+++ desktop/src/ru/hotboyzz/desktop/entities/KoreanManEntity.java
@@ -0,0 +1,7 @@
+package ru.hotboyzz.desktop.entities;
+
+/**
+ * @author Velkonost
+ */
+public class KoreanManEntity {
+}
